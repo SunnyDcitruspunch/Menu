af5c476f1a9f0c6cbe75064e7fac85073a8ee546
SunnyDcitruspunch <wiwisun18@gmail.com>
2019-10-03 17:31:00 -0600

    init commit

---
+++ MenuHW/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MenuHW.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Exit()
+        {
+            return new RedirectResult("https://www.byu.edu/");
+        }
+
+    }
+}
