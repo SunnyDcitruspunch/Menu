d0e451ece5265d265b2b1360b661718786f32884
SunnyDcitruspunch <wiwisun18@gmail.com>
2019-10-08 11:00:17 -0600

    switched to sunnyd branch

--- MenuHW/Controllers/QueueController.cs
+++ MenuHW/Controllers/QueueController.cs
@@ -8,10 +8,43 @@ namespace MenuHW.Controllers
 {
     public class QueueController : Controller
     {
+        static Queue<string> myQueue = new Queue<string>();
+
         // GET: Queue
         public ActionResult Index()
         {
+            ViewBag.MyQueue = myQueue;
             return View();
+        }
+
+        //add one entry in myQueue
+        public ActionResult AddOne()
+        {
+            myQueue.Enqueue("New Entry " + (myQueue.Count + 1));
+            ViewBag.MyQueue = myQueue;
+            return View("Index");
+        }
+
+        //clear previous entries and add 2000 new entries
+        public ActionResult AddAlot()
+        {
+            myQueue.Clear();
+
+            for (var i = 0; i < 2000; i++)
+            {
+                myQueue.Enqueue("New Entry " + i);
+            }
+
+            ViewBag.MyQueue = myQueue;
+            return View("Index");
+        }
+
+        //delete all data in queue
+        public ActionResult DeleteAll()
+        {
+            myQueue.Clear();
+            ViewBag.MyQueue = myQueue;
+            return View("Index");
         }
     }
 }
--- MenuHW/Controllers/StackController.cs
+++ MenuHW/Controllers/StackController.cs
@@ -8,10 +8,45 @@ namespace MenuHW.Controllers
 {
     public class StackController : Controller
     {
+        static Stack<string> myStack = new Stack<string>();
+
         // GET: Stack
         public ActionResult Index()
         {
+            ViewBag.MyStack = myStack;
             return View();
+        }
+
+        //add one entry in myStack
+        public ActionResult AddOne()
+        {
+            myStack.Push("New Entry " + (myStack.Count + 1));
+            ViewBag.MyStack = myStack;
+            return View("Index");
+        }
+
+        //clear previous entries and add 2000 new entries
+        public ActionResult AddAlot()
+        {
+            myStack.Clear();
+
+            for (var i = 0; i < 2000; i++)
+            {
+                myStack.Push("New Entry " + Convert.ToString(i++));
+            }
+
+            ViewBag.MyStack = myStack;
+
+            return View("Index");
+        }
+
+        //delete all data in stack
+        public ActionResult DeleteAll()
+        {
+            myStack.Clear();
+            ViewBag.MyStack = myStack;
+
+            return View("Index");
         }
     }
 }
